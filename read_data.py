d4da82c5fa243cf3a9e5fb728b5971fed0db742e
aadel <aadel@umd.edu>
2022-04-02 18:05:29 -0400

    bug fix in shuffling data

--- read_data.py
+++ read_data.py
@@ -29,13 +29,13 @@ def load_dataset(full_path):
 	return X, y
 
 def shuffle_and_split(X,Y, split):
-	idx = np.random.rand(*X.shape).argsort(axis=0)
-	X = np.take_along_axis(X,idx,axis=0)
-	idx = np.random.rand(*Y.shape).argsort(axis=0)
-	Y = np.take_along_axis(Y,idx,axis=0)
+	idx = np.arange(X.shape[0])
+	np.random.shuffle(idx)
+	X = X[idx,...]
+	Y = Y[idx,...]
 	train_split = np.ceil(X.shape[0]*split).astype(int)
 	test_split = X.shape[0]-train_split
-	X_train,Y_train, X_test, Y_test = X[0:train_split], Y[0:train_split],X[0:test_split], Y[0:test_split]
+	X_train,Y_train, X_test, Y_test = X[0:train_split], Y[0:train_split],X[test_split:], Y[test_split:]
 	return X_train,Y_train, X_test, Y_test
 
 
