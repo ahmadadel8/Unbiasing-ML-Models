23c3a8613609bb965a5aa67d44c879cb9f52239a
aadel <aadel@umd.edu>
2022-04-21 13:14:14 -0400

    accuracy constraint and p rule

--- loss.py
+++ loss.py
@@ -1,11 +1,26 @@
 import tensorflow as tf
+def fairness_constaint_mse(model_out, protected_attribute, y_true):
+    lam = 0
+    loss_fn = tf.keras.losses.BinaryCrossentropy()
+    constraint = tf.reduce_mean(tf.cast(protected_attribute - tf.reduce_mean(protected_attribute), tf.float32) * model_out)
+    y_true = tf.cast(y_true, tf.float32)
+    loss = loss_fn(model_out, y_true)
+    return loss #+ lam* constraint
+
+def p_rule(model_out, protected_attribute):
+    threshold = 0.5
 
 
-def accuracy_constaint(model_out, protected_feature_batch):
-    constraint = tf.reduce_mean((protected_feature_batch - tf.reduce_mean(protected_feature_batch)) * model_out)
-    return constraint
+    protected_idx = tf.where(tf.equal(protected_attribute, 0))
+    protected_model_outs = tf.gather(model_out, protected_idx)
+    protected_model_outs = tf.where(protected_model_outs>threshold, 1, 0)
+    protected_accepted_per = tf.math.reduce_sum(protected_model_outs)/tf.shape(protected_idx)[0]
 
-def p_relu():
-    # protected_idx = tf.where(tf.equal(protected_feature_batch, 0))
-    # protected_model_outs = tf.gather(model_out, protected_idx)
-    pass
+    non_protected_idx = tf.where(tf.equal(protected_attribute, 1))
+    non_protected_model_outs = tf.gather(model_out, non_protected_idx)
+    non_protected_model_outs = tf.where(non_protected_model_outs>threshold, 1, 0)
+    non_protected_accepted_per = tf.math.reduce_sum(non_protected_model_outs)/tf.shape(non_protected_idx)[0]
+
+    p_value = protected_accepted_per/non_protected_accepted_per
+
+    return p_value
