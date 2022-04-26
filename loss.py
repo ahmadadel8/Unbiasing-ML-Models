b74c53c612bc3f5a83cf1a4024c006bb388e9f4e
aadel <aadel@umd.edu>
2022-04-26 15:24:37 -0400

    cleaninig up prule, spearating loss return

--- loss.py
+++ loss.py
@@ -1,24 +1,23 @@
 import tensorflow as tf
 def fairness_constaint_bce(model_out, protected_attribute, y_true):
-    lam = 1
     loss_fn = tf.keras.losses.BinaryCrossentropy()
     constraint = tf.reduce_mean(tf.cast(protected_attribute - tf.reduce_mean(protected_attribute), tf.float32) * model_out)
     y_true = tf.cast(y_true, tf.float32)
     loss = loss_fn(y_true,model_out)
-    return loss + lam* tf.math.abs(constraint)
+    return loss, tf.math.abs(constraint)
 
 def p_rule(model_out, protected_attribute):
-    threshold = 0.9
+    threshold = 0.5
     epsilon = 1e-5
+    model_out = tf.where(model_out>threshold, 1, 0)
     protected_attribute= tf.reshape(protected_attribute, (-1))
+    
     protected_idx = tf.where(tf.equal(protected_attribute, 0))
     protected_model_outs = tf.gather(model_out, protected_idx)        
-    protected_model_outs = tf.where(protected_model_outs>threshold, 1, 0)
     protected_accepted_per = tf.math.reduce_sum(protected_model_outs)/tf.shape(protected_idx)[0]
 
     non_protected_idx = tf.where(tf.equal(protected_attribute, 1))
     non_protected_model_outs = tf.gather(model_out, non_protected_idx)
-    non_protected_model_outs = tf.where(non_protected_model_outs>threshold, 1, 0)
     non_protected_accepted_per = tf.math.reduce_sum(non_protected_model_outs)/tf.shape(non_protected_idx)[0]
 
     p_value = (protected_accepted_per+epsilon)/(non_protected_accepted_per+epsilon)
